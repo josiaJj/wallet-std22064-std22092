0145eec8f32d1c35905bf1cf431105e8635b31fa
josiaJj <hei.josia@gmail.com>
2023-12-14 11:40:19 +0300

    feat: add Database creation to resources

---
+++ resources/create_database.sql
@@ -0,0 +1,6 @@
+SELECT 'hei_wallet' AS database_name
+    WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'hei_wallet');
+
+CREATE DATABASE hei_wallet;
+
+\c hei_wallet;
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import repository.DBConnection;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        DBConnection.getConnection();
     }
 }
