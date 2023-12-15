e2c4f2dd065a28156658f7d4b43a4c3063e55a7a
josiaJj <hei.josia@gmail.com>
2023-12-15 11:41:26 +0300

    feat: add findAll method for Account

--- src/repository/AccountRepositoryJdbc.java
+++ src/repository/AccountRepositoryJdbc.java
@@ -1,16 +1,55 @@
 package repository;
 
 import model.Account;
+import model.AccountType;
+import model.Currency;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class AccountRepositoryJdbc implements AccountRepository{
+public class AccountRepositoryJdbc implements AccountRepository, CrudOperationsBases<Account>{
     public Account findById(int id) {
         return null;
     }
+    @Override
+    public List<Account> findAll() throws SQLException {
+        String sql = "SELECT\n" +
+                "    a.id AS account_id,\n" +
+                "    a.name AS account_name,\n" +
+                "    a.balance AS account_balance,\n" +
+                "    c.name AS currency_name,\n" +
+                "    account_type\n" +
+                "FROM\n" +
+                "    accounts a\n" +
+                "JOIN\n" +
+                "    currency c ON a.currency_id = c.id;";
+        Account account = new Account();
+        List<Account> allAccounts = new ArrayList<>();
+        ResultSet resultSet = DBConnection.getConnection().prepareStatement(sql).executeQuery();
+        while (resultSet.next()){
+            account.setId(resultSet.getInt("account_id"));
+            account.setName(resultSet.getString("account_name"));
+            account.setBalance(resultSet.getDouble("account_balance"));
+            account.setCurrency((Currency) resultSet.getObject("currency_name"));
+            account.setAccountType((AccountType) resultSet.getObject("account_type"));
+            allAccounts.add(account);
+        }
+        return allAccounts;
+    }
 
+    @Override
+    public List<Account> saveAll(List<Account> toSave) {
+        return null;
+    }
+
+    @Override
+    public Account save(Account toSave) {
+        return null;
+    }
     public void updateAccountBalance(Account account) {
         String query = "UPDATE account SET balance = ? WHERE id = ?";
         try (Connection connection = DBConnection.getConnection();
@@ -23,5 +62,4 @@ public class AccountRepositoryJdbc implements AccountRepository{
             throw new RuntimeException(e);
         }
     }
-
 }
--- src/repository/CrudOperationsBases.java
+++ src/repository/CrudOperationsBases.java
@@ -1,9 +1,10 @@
 package repository;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface CrudOperationsBases<T> {
-    List<T> findAll();
+    List<T> findAll() throws SQLException;
     List<T> saveAll(List<T> toSave);
     T save(T toSave);
 }
