98f45adeaa169dc8bf0838ab32b986517fc638ae
josiaJj <hei.josia@gmail.com>
2023-12-15 11:58:10 +0300

    feat: Add Balance model

---
+++ src/model/Balance.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Balance {
+    private int id_account;
+    private LocalDateTime datetime;
+    private BigDecimal value;
+}
--- src/repository/TransactionRepositoryJdbc.java
+++ src/repository/TransactionRepositoryJdbc.java
@@ -25,4 +25,29 @@ public class TransactionRepositoryJdbc implements TransactionRepository{
             throw new RuntimeException(e);
         }
     }
+    public static BalanceModel getBalanceAtDateTime(AccountModel accountModel, Timestamp transaction_date) {
+        try {
+            String sql = String.format(
+                    "SELECT * FROM \"%s\" WHERE %s = ? AND %s <= ? ORDER BY %s DESC LIMIT 1",
+                    BalanceModel.TABLE_NAME,
+                    BalanceModel.ID_ACCOUNT,
+                    BalanceModel.DATETIME,
+                    BalanceModel.DATETIME
+            );
+            PreparedStatement preparedStatement = connectionDB.getConnection().prepareStatement(sql);
+            preparedStatement.setInt(1, accountModel.getId());
+            preparedStatement.setTimestamp(2, Timestamp.valueOf(transaction_date.toLocalDateTime()));
+            ResultSet resultSet = preparedStatement.executeQuery();
+            BalanceModel balanceModel = new BalanceModel();
+            if (resultSet.next()) {
+                balanceModel.setId_account(resultSet.getInt(BalanceModel.ID_ACCOUNT));
+                balanceModel.setValue(resultSet.getBigDecimal(BalanceModel.VALUE));
+                balanceModel.setDatetime(resultSet.getTimestamp(BalanceModel.DATETIME).toLocalDateTime());
+                return balanceModel;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
 }
