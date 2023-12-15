8fe0cfaf3ca07467299f655cc73bf4669dd481c6
josiaJj <hei.josia@gmail.com>
2023-12-15 14:21:57 +0300

    feat: add CurrencyValue model

---
+++ src/model/CurrencyValue.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class CurrencyValue {
+    private int id;
+    private String currencySrc;
+    private String currencyDest;
+    private Double amount;
+    private LocalDateTime datetime;
+}
