1589937a2d6430a479c24ec80d16d2d0b5540702
josiaJj <hei.josia@gmail.com>
2023-12-17 22:10:27 +0300

    fix: implement performTransaction method to persist to database

--- src/model/TransactionType.java
+++ src/model/TransactionType.java
@@ -1,5 +1,12 @@
 package model;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
 public enum TransactionType {
     Credit, Debit;
 }
--- src/service/TransactionServiceImpl.java
+++ src/service/TransactionServiceImpl.java
@@ -5,15 +5,18 @@ import model.AccountType;
 import model.Transaction;
 import model.TransactionType;
 import repository.AccountRepositoryJdbc;
+import repository.TransactionRepositoryJdbc;
 
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 
 public class TransactionServiceImpl implements TransactionService{
     private final AccountRepositoryJdbc accountRepositoryJdbc;
+    private final TransactionRepositoryJdbc transactionRepositoryJdbc;
 
-    public TransactionServiceImpl(AccountRepositoryJdbc accountRepositoryJdbc) {
+    public TransactionServiceImpl(AccountRepositoryJdbc accountRepositoryJdbc, TransactionRepositoryJdbc transactionRepositoryJdbc) {
         this.accountRepositoryJdbc = accountRepositoryJdbc;
+        this.transactionRepositoryJdbc = transactionRepositoryJdbc;
     }
 
     @Override
@@ -21,24 +24,25 @@ public class TransactionServiceImpl implements TransactionService{
         Account account = new Account();
 
         Transaction transaction = new Transaction();
-        transaction.setId(generateTransactionId());
         transaction.setLabel("Transaction");
         transaction.setAmount(amount);
-        transaction.setDateTime(LocalDateTime.now()); // Utilisation de LocalDateTime pour la date actuelle
+        transaction.setDateTime(LocalDateTime.now());
         transaction.setTransactionType(transactionType);
+        transaction.setAccountId(accountId);
 
         if (TransactionType.Debit.equals(transactionType) && !AccountType.Bank.equals(account.getAccountType())) {
             if (account.getBalance() < amount) {
                 throw new IllegalArgumentException("Insufficient funds for debit transaction.");
             }
         }
-        if (TransactionType.Credit.equals(transactionType) || AccountType.Bank.equals(account.getAccountType())) {
+        if (TransactionType.Credit.equals(transactionType) && AccountType.Bank.equals(account.getAccountType())) {
             account.setBalance(account.getBalance() + amount);
         } else {
             account.setBalance(account.getBalance() - amount);
         }
 
         account.getListTransactions().add(transaction);
+        transactionRepositoryJdbc.insertTransaction(transaction, accountId);
         accountRepositoryJdbc.updateAccountBalance(account);
 
         return account;
