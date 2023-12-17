b2f70bb6566c22f8717e96ac261a9a0840b3f967
josiaJj <hei.josia@gmail.com>
2023-12-17 22:20:16 +0300

    refactor: move getBalanceAtDatetime method to BlanceRepository

--- src/repository/TransactionRepositoryJdbc.java
+++ src/repository/TransactionRepositoryJdbc.java
@@ -25,29 +25,5 @@ public class TransactionRepositoryJdbc implements TransactionRepository{
             throw new RuntimeException(e);
         }
     }
-    public static Balance getBalanceAtDateTime(Account accountModel, Timestamp transaction_date) {
-        try {
-            String sql = String.format(
-                    "SELECT * FROM \"%s\" WHERE %s = ? AND %s <= ? ORDER BY %s DESC LIMIT 1",
-                    Balance.TABLE_NAME,
-                    Balance.ID_ACCOUNT,
-                    Balance.DATETIME,
-                    Balance.DATETIME
-            );
-            PreparedStatement preparedStatement = connectionDB.getConnection().prepareStatement(sql);
-            preparedStatement.setInt(1, accountModel.getId());
-            preparedStatement.setTimestamp(2, Timestamp.valueOf(transaction_date.toLocalDateTime()));
-            ResultSet resultSet = preparedStatement.executeQuery();
-            BalanceModel balanceModel = new BalanceModel();
-            if (resultSet.next()) {
-                balanceModel.setId_account(resultSet.getInt(BalanceModel.ID_ACCOUNT));
-                balanceModel.setValue(resultSet.getBigDecimal(BalanceModel.VALUE));
-                balanceModel.setDatetime(resultSet.getTimestamp(BalanceModel.DATETIME).toLocalDateTime());
-                return balanceModel;
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return null;
-    }
+
 }
