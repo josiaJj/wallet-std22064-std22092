73f8bf958a8e8cc078f8961a851e19a3aafc7293
tsanta-niaina-MT <hei.tsantaniaina@gmail.com>
2024-01-05 20:46:23 +0100

    creating specific categories for expenses and cash receipts

---
+++ src/model/Categorie.java
@@ -0,0 +1,30 @@
+package model;
+
+public class Categorie {
+
+    private int id ;
+    private String nom ;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    @Override
+    public String toString() {
+        return "Categorie{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                '}';
+    }
+}
