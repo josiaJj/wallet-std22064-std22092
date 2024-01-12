08ccf8284c08a280f0e8253bf586b44637f71aa1
josiaJj <hei.josia@gmail.com>
2024-01-12 09:15:32 +0300

    rename attributs name in accounts database

--- resources/create_table_accounts.sql
+++ resources/create_table_accounts.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS accounts (
     id SERIAL PRIMARY KEY,
     "name" VARCHAR(255) NOT NULL,
-    creation_date TIMESTAMP DEFAULT current_timestamp;
     balance NUMERIC NOT NULL,
+    updated_balance TIMESTAMP DEFAULT current_timestamp,
     account_type VARCHAR(20) NOT NULL,
     currency_id INT,
     FOREIGN KEY (currency_id) REFERENCES currency(id)
